cdd11def2a29595d11dc878abd13330b84634a95
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-05-10 21:55:44 -0400

    Update CoreNLP dependencies and add test file

---
+++ src/main/java/edu/rosehulman/zhouz2/Agent.java
@@ -0,0 +1,21 @@
+package edu.rosehulman.zhouz2;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Joe.
+ *         Created May 10, 2017.
+ */
+public class Agent {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+		System.out.println("It works!");
+	}
+
+}
