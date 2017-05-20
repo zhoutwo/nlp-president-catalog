eba72e8b34a8cd889c7968d561afc4d455ebf80f
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-05-20 14:56:37 -0400

    Implement getBody text

--- src/test/java/edu/rosehulman/zhouz2/PhantomJSWikiHTMLReaderTest.java
+++ src/test/java/edu/rosehulman/zhouz2/PhantomJSWikiHTMLReaderTest.java
@@ -3,6 +3,11 @@ package edu.rosehulman.zhouz2;
 import org.junit.jupiter.api.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,11 +33,20 @@ public class PhantomJSWikiHTMLReaderTest {
   }
 
   @Test
-  public void testGetBody() {
+  public void testGetBody() throws IOException {
     Throwable exception = assertThrows(IllegalStateException.class, () -> {
       reader.getBody();
     });
     assertEquals("HTML file path hasn't been set!", exception.getMessage());
+    File bodyText = new File("src/test/java/edu/rosehulman/zhouz2/PhantomJSWikiHTMLReaderBody.txt");
+    StringBuffer buffer = new StringBuffer(64711);
+    FileReader fileReader = new FileReader(bodyText);
+    int nextChar;
+    while ((nextChar = fileReader.read()) != -1) {
+      buffer.append((char) nextChar);
+    }
+    reader.setWikiHTMLFile(new File("data/html/Adams.html"));
+    assertEquals(buffer.toString(), reader.getBody());
   }
 
   @Test
