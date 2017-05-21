0cf8e32651e675aba9a204ac6947570d7e3f946f
MainTankJerry <qiuz@rose-hulman.edu>
2017-05-20 20:42:23 -0400

    Passing to Zhou for further progress

--- src/main/java/edu/rosehulman/zhouz2/Agent.java
+++ src/main/java/edu/rosehulman/zhouz2/Agent.java
@@ -1,4 +1,5 @@
 package edu.rosehulman.zhouz2;
+
 import edu.stanford.nlp.dcoref.*;
 import edu.stanford.nlp.ling.CoreAnnotations.*;
 import edu.stanford.nlp.ling.*;
@@ -26,15 +27,12 @@ public class Agent {
   public static void main(String[] args) {
     // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
 
-    StanfordCoreNLP pipeline = new StanfordCoreNLP(
-      PropertiesUtils.asProperties(
-        "annotators", "tokenize,ssplit,pos,lemma,parse,ner,natlog,dcoref",
-        "tokenize.language", "en"));
+    StanfordCoreNLP pipeline = new StanfordCoreNLP(PropertiesUtils.asProperties("annotators", "tokenize,ssplit,pos,lemma,parse,ner,natlog,dcoref", "tokenize.language", "en"));
     String text;
     //if (args.length != 0 && args[0].equals("-all")) {
-    if (true){
+    if (false) {
       //Fill in later
-      text = "Jerry was born May 20th, 1996.";
+      text = "Jerry was born on May 20, 1996.";
     } else {
       // Test Ghost Driver
       IWikiHTMLReader htmlReader = new PhantomJSWikiHTMLReader();
@@ -48,50 +46,36 @@ public class Agent {
     // create an empty Annotation just with the given text
     Annotation document = new Annotation(text);
 
-    // run all Annotators on this text
     pipeline.annotate(document);
-    // these are all the sentences in this document
-// a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+
     List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 
     for (CoreMap sentence : sentences) {
-      // traversing the words in the current sentence
-      // a CoreLabel is a CoreMap with additional token-specific methods
-      for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-        // this is the text of the token
-        String word = token.get(TextAnnotation.class);
-        // this is the POS tag of the token
-        String pos = token.get(PartOfSpeechAnnotation.class);
-        // this is the NER label of the token
-        String ne = token.get(NamedEntityTagAnnotation.class);
-
-        String ca = token.get(CategoryAnnotation.class);
-
-        String la = token.get(LemmaAnnotation.class);
-
-        int ia = token.get(IndexAnnotation.class);
-
-        //System.out.println(word + "-" + pos + "-" + ne + "-" + ca + "-" + la + "-" + ia);
-      }
-      // this is the parse tree of the current sentence
       Tree tree = sentence.get(TreeAnnotation.class);
       System.out.println(tree.toString());
-
-
-      // this is the Stanford dependency graph of the current sentence
-      // We think this is useless since it is not good at telling the difference between object and verb
-      //SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-      //System.out.println(dependencies);
     }
 
-    // This is the coreference link graph
-    // Each chain stores a set of mentions that link to each other,
-    // along with a method for getting the most representative mention
-    // Both sentence and token offsets start at 1!
-    Map<Integer, CorefChain> graph =
-            document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
-  }
+    Scanner reader = new Scanner(System.in);
+    System.out.println("Enter a statement: ");
+    String n = reader.next();
+    ArrayList<String> lemmas = new ArrayList<>();
+    while (n.equals("q")) {
+      for (CoreMap sentence : sentences) {
 
+        for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+          String la = token.get(LemmaAnnotation.class);
+          
+        }
+        // this is the parse tree of the current sentence
+        Tree tree = sentence.get(TreeAnnotation.class);
+
+      }
+
+
+      System.out.println("Enter a statement: ");
+      n = reader.next();
+    }
+  }
 
 
 }
