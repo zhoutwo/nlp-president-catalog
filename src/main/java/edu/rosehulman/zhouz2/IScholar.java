b6a93020b8489cc6577ef56038b55837c66e12c6
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-05-20 21:13:50 -0400

    Update IAgent specification

--- src/main/java/edu/rosehulman/zhouz2/IScholar.java
+++ src/main/java/edu/rosehulman/zhouz2/IScholar.java
@@ -1,14 +1,19 @@
 package edu.rosehulman.zhouz2;
 
+import edu.stanford.nlp.trees.Tree;
+
+import java.util.*;
+
 /**
  * Created by Zhou Zhou on 5/20/17.
  */
 public interface IScholar {
   /**
    * Add source for the scholar to parse. You can call this method several times before calling testStatement()
+   * @param name Name of the person the source is related to
    * @param source Source texts to learn and parse
    */
-  public void parseText(String source);
+  public void parseText(String name, String source);
 
   /**
    * Verify a statement whether it's known to be true or not
@@ -16,4 +21,17 @@ public interface IScholar {
    * @return True if the statement is true. False if unknown or false
    */
   public boolean testStatement(String statement);
+
+  /**
+   * Return all parse trees under the specified name
+   * @param name Name of the person to which the parse trees belong
+   * @return All parse trees registered under the person with the name
+   */
+  public List<Tree> getParseTreeByName(String name);
+
+  /**
+   * Return all parsgie trees sorted by name
+   * @return A map from name to all parse trees related to the person with that name
+   */
+  public Map<String, List<Tree>> getParseTrees();
 }
