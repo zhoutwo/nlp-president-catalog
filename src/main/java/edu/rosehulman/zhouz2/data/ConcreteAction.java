22538f40eeaad773bf9a8c8fc881c2e5a7496e89
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-05-23 10:54:31 -0400

    Implement ConcreteAction

--- src/main/java/edu/rosehulman/zhouz2/data/ConcreteAction.java
+++ src/main/java/edu/rosehulman/zhouz2/data/ConcreteAction.java
@@ -4,23 +4,52 @@ package edu.rosehulman.zhouz2.data;
  * Created by Zhou Zhou on 5/22/17.
  */
 public class ConcreteAction implements IAction {
-  @Override
-  public String getName() {
-    return null;
+  private String name;
+
+  public ConcreteAction() {
+    this(null);
+  }
+
+  public ConcreteAction(String n) {
+    name = n;
   }
 
   @Override
-  public void setName(String name) {
+  public String getName() {
+    return name;
+  }
 
+  @Override
+  public void setName(String n) {
+    name = n;
   }
 
   @Override
   public boolean match(IMatchable m) {
+    // TODO Implement this
     return false;
+  }
+
+  @Override
+  public boolean matchByName(String n) {
+    return name.equals(n);
   }
 
   @Override
   public boolean isUnknownValue() {
     return false;
+  }
+
+  @Override
+  public SpecificityComparisonOutput isMoreSpecificThan(IMatchable m) {
+    if (!(m instanceof IAction)) {
+      return SpecificityComparisonOutput.NOT_RELATED;
+    } else if (m.isUnknownValue()) {
+      return SpecificityComparisonOutput.MORE_SPECIFIC;
+    } else if (match(m)) {
+      return SpecificityComparisonOutput.EQUALLY_SPECIFIC;
+    } else {
+      return SpecificityComparisonOutput.NOT_RELATED;
+    }
   }
 }
